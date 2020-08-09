ceefae9b7d6fd17243e786b28b4b9ae5dfd10447
“mbartlett413” <Mbartlett413@me.com>
2020-08-09 13:14:11 -0400

    small tweaks

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,5 @@
 class LandingPageController < ApplicationController
-
+before_action :authenticate_user!, only:[:admin_page]
 	#This will be the landing page for the user
 	#Display everything from here
 	def index
